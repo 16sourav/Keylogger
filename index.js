949f1e589f2f1dd14cd82c481b86dc0e0d1b2042
16sourav <126350102+16sourav@users.noreply.github.com>
2025-01-04 19:11:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const stopBtn = document.getElementById('stop-btn');
 startBtn.addEventListener("click",()=>{
     document.addEventListener("keydown",handleDown);
     document.addEventListener("keyup",handleUp);
+    startBtn.disabled;
 })
 
 stopBtn.addEventListener("click",()=>{
     document.removeEventListener("keydown",handleDown);
     document.removeEventListener("keyup",handleUp);
-    logDiv.textContent="";
-    stateDiv.textContent=" ";
+    logDiv.textContent="Press Start to see Key Logging";
+    stateDiv.textContent="Press Start to see Key Logging";
+    stopBtn.disabled;
 })
 
 function handleDown(e){
